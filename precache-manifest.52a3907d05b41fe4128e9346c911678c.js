239b9d2562c57edc2a942936e3b6505df807fd3a
georgeraja <rajageorge971@gmail.com>
2020-08-12 15:37:44 -0700

    Updates

---
+++ precache-manifest.52a3907d05b41fe4128e9346c911678c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52ebe508e8bd12ddab8f950850e9153",
+    "url": "/index.html"
+  },
+  {
+    "revision": "427e3d1fe0d7677c5190",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2f427034a5b98bb9393f",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "427e3d1fe0d7677c5190",
+    "url": "/static/js/2.9a362538.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.9a362538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f427034a5b98bb9393f",
+    "url": "/static/js/main.3cbd3baf.chunk.js"
+  },
+  {
+    "revision": "183db94de12628515374",
+    "url": "/static/js/runtime-main.f0911912.js"
+  }
+]);
