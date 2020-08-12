c08d66039732d34e26bae792d2f39af9d5ad0f4d
georgeraja <rajageorge971@gmail.com>
2020-08-12 15:29:29 -0700

    final commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function about() {
+  return (
+    <div>
+      <h1 className="display-4">About Contact manager</h1>
+      <p>Simple App to manage contacts</p>
+      <p className="text-secondary">Version 1.0.0</p>
+    </div>
+  );
+}
